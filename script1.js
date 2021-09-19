15c45632dde3361a0cc898c1f28763dfc793431a
Gladiator <fightfreeeak@gmail.com>
2021-09-19 18:28:46 +0530

    added animation on keypress

--- script1.js
+++ script1.js
@@ -1,27 +1,41 @@
 window.onload = function(){
+    const tom1 = document.querySelector('.tom1');
+    const tom2 = document.querySelector('.tom2');
+    const tom3 = document.querySelector('.tom3');
+    const tom4 = document.querySelector('.tom4');
+    const kick = document.querySelector('.kick');
+    const snare = document.querySelector('.snare');
+    const crash= document.querySelector('.crash');
     const drums = document.querySelectorAll('.drum');
 
     document.addEventListener('keyup', function(e){
         let key = e.key;
         if(key === "w"){
+            addHighlight(tom1.classList);
             new Audio('./sounds/tom-1.mp3').play();
         }
         if(key === "a"){
+            addHighlight(tom2.classList);
             new Audio('./sounds/tom-2.mp3').play()
         }
         if(key === "s"){
+            addHighlight(tom3.classList);
             new Audio('./sounds/tom-3.mp3').play()
         }
         if(key === "d"){
+            addHighlight(tom4.classList);
             new Audio('./sounds/tom-4.mp3').play()
         }
         if(key === "j"){
+            addHighlight(kick.classList);
             new Audio('./sounds/kick-bass.mp3').play()
         }
         if(key === "k"){
+            addHighlight(snare.classList);
             new Audio('./sounds/snare.mp3').play()
         }
         if(key === "l"){
+            addHighlight(crash.classList);
             new Audio('./sounds/crash.mp3').play()
         }
     })
